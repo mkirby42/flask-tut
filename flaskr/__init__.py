da601a0f8439240f8809dc4fd457bf290f9547ce
Mkirby42 <mkirby3@angelo.edu>
2020-08-14 14:05:37 -0500

    Create App

---
+++ flaskr/__init__.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask
+
+def create_app(test_config = None):
+    """Flask Application Factory"""
+
+    # Create and configure application
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        database = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # A simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello world!'
+
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint = 'index')
+
+    return app
